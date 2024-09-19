a5fcf97ea49d945afd22bfcc8125dba7c8aef060
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-09-20 00:04:09 +0200

    Add __all__ declaration and improve module exports

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,10 +1,11 @@
 from .users import Users
 from .memories import Memories
 from .database import User, Follower, Memory, Comment, LikeList, get_session, create_database, create_tables
-
+from modules.comments import Comments_class
 
 __all__ = [
     'Users', 'Memories', 'get_session',
     'User', 'Memory', 'Comment', 'LikeList',
-    'Follower', 'create_database', 'create_tables'
+    'Follower', 'create_database', 'create_tables',
+    'Comments_class',
 ]
